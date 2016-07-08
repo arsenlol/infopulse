c939cf39a0dd64f546a675f82d9c7e54185ec6f0
arsenlol <arsen325@gmail.com>
2016-07-08 10:42:22 +0300

    json test

--- main.js
+++ main.js
@@ -136,3 +136,24 @@ for (var i = 0; i < item.length; i++){
 		}
 	});
 }
+
+function fetchJSONFile(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send(); 
+}
+
+// this requests the file and executes a callback with the parsed result once
+//   it is available
+fetchJSONFile('data.json', function(data){
+    // do something with your data
+    console.log(data);
+});
