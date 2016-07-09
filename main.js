51f1e4b39c6a8af6867cfbc13e74271f5c2f6d04
Arseniy <arsen325@gmail.com>
2016-07-09 11:09:37 +0300

    json trial

--- main.js
+++ main.js
@@ -91,7 +91,25 @@ function refreshSum(){
 }
 
 // GET CATALOG DATA AND BUILD CATALOG
-var catalog = JSON.parse(catalogData);
+function fetchJSONFile(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send(); 
+}
+
+var catalog;
+fetchJSONFile('data.json', function(data){
+    catalog = JSON.parse(data);
+});
+
 function buildCatalog(){
 
 	var samples = [];
@@ -191,23 +209,3 @@ popup.addEventListener('click',function(event){
 	});
 
 
-// function fetchJSONFile(path, callback) {
-//     var httpRequest = new XMLHttpRequest();
-//     httpRequest.onreadystatechange = function() {
-//         if (httpRequest.readyState === 4) {
-//             if (httpRequest.status === 200) {
-//                 var data = JSON.parse(httpRequest.responseText);
-//                 if (callback) callback(data);
-//             }
-//         }
-//     };
-//     httpRequest.open('GET', path);
-//     httpRequest.send(); 
-// }
-
-// // this requests the file and executes a callback with the parsed result once
-// //   it is available
-// fetchJSONFile('data.json', function(data){
-//     // do something with your data
-//     console.log(data);
-// });
